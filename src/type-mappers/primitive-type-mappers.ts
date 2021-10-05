ef2770e269bdacf543669df5cb37274627f5d54a
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-10-04 23:45:00 -0400

    Update string type mapper

--- src/type-mappers/primitive-type-mappers.ts
+++ src/type-mappers/primitive-type-mappers.ts
@@ -1,7 +1,7 @@
 import { TypeMapper } from '..';
 import { StringUtils } from '../utils';
 
-let stringTm: TypeMapper<string> = (jsonValue: any) => StringUtils.join(jsonValue, '\n');
+let stringTm: TypeMapper<string> = (jsonValue: any) => StringUtils.join(String(jsonValue), '\n');
 let booleanTm: TypeMapper<boolean> = (jsonValue: any) => Boolean(jsonValue);
 let numberTm: TypeMapper<number> = (jsonValue: any) => Number(jsonValue);
 let bigIntTm: TypeMapper<BigInt> = (jsonValue: any) => BigInt(jsonValue);
