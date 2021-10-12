d09dd486dedc2626a4b8db70682d8b310c931b15
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-10-11 22:43:40 -0400

    Allow single-line regex

--- src/type-mappers/common-type-mappers.ts
+++ src/type-mappers/common-type-mappers.ts
@@ -3,8 +3,18 @@ import { TypeMapper } from '..';
 
 let dateTm: TypeMapper<Date> = (jsonValue: any) => new Date(jsonValue);
 
-let regExpTm: TypeMapper<RegExp> = (jsonValue: any) =>
-    new RegExp(jsonValue.pattern, jsonValue.flags);
+let regExpTm: TypeMapper<RegExp> = (jsonValue: any) => {
+    if (typeof jsonValue === 'string') {
+        let match = /^\/(.*)\/([^\/]*)$/.exec(jsonValue);
+        if (!match) {
+            return new RegExp('');
+        }
+
+        return new RegExp(match[1], match[2]);
+    } else {
+        return new RegExp(jsonValue.pattern, jsonValue.flags);
+    }
+};
 
 let urlTm: TypeMapper<URL> = (jsonValue: any) => new URL(jsonValue);
 
