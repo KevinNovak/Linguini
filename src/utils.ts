aa6546f9c206dab92ddafd98dea3065067acbca7
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-10-12 21:19:30 -0400

    Add tsdocs for Utils#join()

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,14 @@
 import { StringUtils } from './utils/';
 
 export class Utils {
+    /**
+     * Joins a string array into a string. Useful for custom Type Mappers.
+     *
+     * @param input - The string array (or string) to join.
+     * @param separator - A separator string to place between items.
+     *
+     * @returns The joined string.
+     */
     public static join(input: string | string[], separator: string): string {
         return StringUtils.join(input, separator);
     }
