09890c0a795811c5256bd9c4746f7998c6412eb6
Scott Bucher <stqlth@gmail.com>
2025-12-04 12:32:17 -0500

    random clean up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-// Main class
 export { Linguini, LinguiniOptions, Variables } from './linguini';
-
-// Types
 export {
     CategoryItems,
     CommonFile,
@@ -10,13 +7,7 @@ export {
     TypeMapper,
     ValueAt,
 } from './models/types';
-
-// Utilities
 export { Utils } from './utils';
 export { TypeMappers } from './type-mappers';
-
-// Errors
 export { LinguiniError } from './models/error-models';
-
-// Loaders (for advanced use)
 export { FileLoader } from './loaders';
